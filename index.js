5d0bdf0f00b52ac1e840798ff6ea77acfc606d29
Maksym Tkach <maksym.tkach@student.kdg.be>
2025-03-05 19:46:39 +0100

    Fix Railway SIGTERM issue

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const { es } = require('date-fns/locale'); // not used?
 const callbackController = require('./controllers/callbackContoller');
 const documentController = require('./controllers/documentController');
 const messageController = require('./controllers/messageController');
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Бот працює!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Сервер запущено на порту ${PORT}`);
+});
 
 const bot = require('./config/botInstance');
 
